75dea570007f79d38c8e48a3f0c728e3fae17a9b
Teresa (Tessa) Rivas <rivas.gomez.teresa@gmail.com>
2025-10-08 14:33:10 -0700

    agregue fondo y shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import './App.css'
+import GradientBlinds from '../src/components/GradientBlinds'
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className='w-screen h-screen flex justify-center items-center bg-black'>
+      <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+        <GradientBlinds
+          gradientColors={['#FF9FFC', '#5227FF']}
+          angle={0}
+          noise={0.3}
+          blindCount={12}
+          blindMinWidth={50}
+          spotlightRadius={0.5}
+          spotlightSoftness={1}
+          spotlightOpacity={1}
+          mouseDampening={0.15}
+          distortAmount={0}
+          shineDirection="left"
+          mixBlendMode="lighten"
+        />
+      </div>
+    </div>
     </>
   )
 }
